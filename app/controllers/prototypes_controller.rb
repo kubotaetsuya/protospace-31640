37fa02e6ac71370440daf57129d467bd5e6fb1db
instructor-salvage <72959612+instructor-salvage@users.noreply.github.com>
2020-11-10 13:53:20 +0900

    デプロイ後、エラー修正
    新規投稿時のエラー修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,8 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :set_prototype, only: [:edit, :show]
+  before_action :move_to_index, only: [:edit]
+
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -20,13 +22,11 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
@@ -51,9 +51,12 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
   
-  def move_to_index
+  def set_prototype
     @prototype = Prototype.find(params[:id])
-    unless @prototype.user_id == current_user.id
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
